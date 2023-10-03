c62238c078a716e0459034d688512b64913934f4
순민 <tkksm2@gmail.com>
2023-10-03 18:08:48 +0900

    fix: next config & ts config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: { unoptimized: true },
+};
 
 module.exports = withContentlayer(nextConfig);
