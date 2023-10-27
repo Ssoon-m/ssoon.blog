dc9fa936965cfbcea3ca4cea3f15ab8801bf706c
Ssoon-m <tkksm2@gmail.com>
2023-10-27 17:52:11 +0900

    feat: add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import BasicLayout from '@/components/layout/BasicLayout';
+
+const NotFoundPage = () => {
+  return (
+    <BasicLayout>
+      <div className="flex-1 flex justify-center items-center">
+        페이지를 찾을 수 없습니다.
+      </div>
+    </BasicLayout>
+  );
+};
+
+export default NotFoundPage;
