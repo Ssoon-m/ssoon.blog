5e4ab77189f4c55625206db4282390a3abcf2592
Ssoon-m <tkksm2@gmail.com>
2023-10-30 12:33:05 +0900

    chore: remove BasicLayout from the notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,8 @@
-import BasicLayout from '@/components/layout/BasicLayout';
-
 const NotFoundPage = () => {
   return (
-    <BasicLayout>
-      <div className="flex-1 flex justify-center items-center">
-        페이지를 찾을 수 없습니다.
-      </div>
-    </BasicLayout>
+    <div className="flex-1 flex justify-center items-center">
+      페이지를 찾을 수 없습니다.
+    </div>
   );
 };
 
