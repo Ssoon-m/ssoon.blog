036c100e0cf77eeffa30958405a9797fd0f4bc7a
순민 <tkksm2@gmail.com>
2024-03-14 19:34:47 +0900

    style: series card style

--- src/app/series/components/SeriesCard.tsx
+++ src/app/series/components/SeriesCard.tsx
@@ -9,12 +9,9 @@ interface SeriesCardProps {
 const SeriesCard = ({ series }: SeriesCardProps) => {
   return (
     <div className="h-full rounded-md overflow-hidden">
-      <div className="p-[2px] w-full h-full rounded-md overflow-hidden bg-gradient-to-r from-indigo-500 to-purple-500">
-        <div className="bg-white dark:bg-zinc-900 rounded-md h-full w-full">
-          <Link
-            href={`/${series.postUrl.replace(/^blog/i, 'series')}`}
-            className="h-ful"
-          >
+      <div className="p-[2px] w-full h-full rounded-md overflow-hidden bg-gradient-to-r from-slate-300 to-slate-500  dark:from-indigo-300 dark:to-indigo-500">
+        <div className=" bg-slate-50 dark:bg-zinc-900 rounded-md h-full w-full">
+          <Link href={`/${series.postUrl.replace(/^blog/i, 'series')}`}>
             <div className="p-3 h-[170px] flex flex-col">
               <div className="pb-3">
                 <h2 className="text-xl font-medium">{series.title}</h2>
@@ -22,7 +19,7 @@ const SeriesCard = ({ series }: SeriesCardProps) => {
               <div className="pb-2 text-gray-600 dark:text-gray-300">
                 <span>{series.description}</span>
               </div>
-              <div className="flex-1 text-gray-300 text-sm text-right flex flex-col justify-end">
+              <div className="flex-1 text-gray-400 text-sm text-right flex flex-col justify-end">
                 마지막 업데이트 : {displayTime(series.date)}
               </div>
             </div>
