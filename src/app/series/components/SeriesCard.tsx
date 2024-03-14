2089ff541c1515b972c40e50c264a00a7be4b0b0
순민 <tkksm2@gmail.com>
2024-03-14 17:54:22 +0900

    style: series darkmode

--- src/app/series/components/SeriesCard.tsx
+++ src/app/series/components/SeriesCard.tsx
@@ -8,9 +8,9 @@ interface SeriesCardProps {
 
 const SeriesCard = ({ series }: SeriesCardProps) => {
   return (
-    <div className="bg-white h-full rounded-md overflow-hidden">
+    <div className="h-full rounded-md overflow-hidden">
       <div className="p-[2px] w-full h-full rounded-md overflow-hidden bg-gradient-to-r from-indigo-500 to-purple-500">
-        <div className="bg-white rounded-md h-full w-full">
+        <div className="bg-white dark:bg-zinc-900 rounded-md h-full w-full">
           <Link
             href={`/${series.postUrl.replace(/^blog/i, 'series')}`}
             className="h-ful"
@@ -19,10 +19,10 @@ const SeriesCard = ({ series }: SeriesCardProps) => {
               <div className="pb-3">
                 <h2 className="text-xl font-medium">{series.title}</h2>
               </div>
-              <div className="pb-2 text-gray-600">
+              <div className="pb-2 text-gray-600 dark:text-gray-300">
                 <span>{series.description}</span>
               </div>
-              <div className="flex-1 text-gray-400 text-sm text-right flex flex-col justify-end">
+              <div className="flex-1 text-gray-300 text-sm text-right flex flex-col justify-end">
                 마지막 업데이트 : {displayTime(series.date)}
               </div>
             </div>
